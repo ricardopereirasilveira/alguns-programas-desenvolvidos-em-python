7b09424e84780ce4dc788ece8122f995d80b1334
RICARDOPEREIRA <ricardopereirasilveira@gmail.com>
2020-03-19 12:08:28 -0300

    Add files via upload

---
+++ encontrando-numeros-primos.py
@@ -0,0 +1,9 @@
+## Dentro de um Range, achar todos os números primos e imprimir ele para o usuário
+## -------------------------------
+## Numero Primo: Todos os números que dão para dividir somente por ele e por 1
+## - - Dividir somente por 2 números
+
+
+
+rangeMenor = int(input("Digite o numero menor: "))
+rangeMaior = int(input("Digite o número maior: "))
