43538c37c4d1bca4236ac1b3b68ec9980be79b02
RICARDOPEREIRA <ricardopereirasilveira@gmail.com>
2020-06-30 12:06:55 -0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-# Classe Bola: Crie uma classe que modele uma bola:
-# Atributos: Cor, circunferência, material
-# Métodos: trocaCor e mostraCor
-
-from bola import Bola
-
-
-bola1 = Bola('branca', '50cm', 'plástico')
-print(bola1.circuferencia)
