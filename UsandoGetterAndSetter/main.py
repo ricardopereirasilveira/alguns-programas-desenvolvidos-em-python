def3cfc8effb53bd58890ed9f9e12fc359a8c8d0
RICARDOPEREIRA <ricardopereirasilveira@gmail.com>
2020-06-30 12:07:22 -0300

    Add files via upload

---
+++ UsandoGetterAndSetter/main.py
@@ -0,0 +1,9 @@
+# Classe Bola: Crie uma classe que modele uma bola:
+# Atributos: Cor, circunferência, material
+# Métodos: trocaCor e mostraCor
+
+from bola import Bola
+
+
+bola1 = Bola('branca', '50cm', 'plástico')
+print(bola1.circuferencia)
